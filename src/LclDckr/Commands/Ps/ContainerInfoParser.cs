c376d4f915d2bce577639844b2f956e84bcb36c2
derrickcrowne <erickeithbrown@gmail.com>
2016-12-05 16:40:23 -0800

    Added support for ps command

---
+++ src/LclDckr/Commands/Ps/ContainerInfoParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LclDckr.Commands.Ps
+{
+    /// <summary>
+    /// Parses output of Ps command
+    /// </summary>
+    internal class ContainerInfoParser
+    {
+        private readonly string[] _expectedHeaders =
+        {
+            "CONTAINER ID",
+            "IMAGE",
+            "COMMAND",
+            "CREATED",
+            "STATUS",
+            "PORTS",
+            "NAMES"
+        };
+
+        private readonly Tuple<int, int>[] _fieldLocations;
+
+        public ContainerInfoParser(string headers)
+        {
+            _fieldLocations = new Tuple<int, int>[_expectedHeaders.Length];
+
+            for (int i = 0; i < _expectedHeaders.Length; i++)
+            {
+                var idRegex = new Regex($"({_expectedHeaders[i]}) *");
+                var match = idRegex.Match(headers);
+                if (!match.Success)
+                {
+                    throw new Exception($"Returned headers from ps command did not match expected headers {headers}");
+                }
+
+                _fieldLocations[i] = Tuple.Create(match.Index, match.Length);
+            }
+        }
+
+        public ContainerInfo Parse(string fields)
+        {
+            Func<int, string> getField = i => fields.Substring(_fieldLocations[i].Item1, i < _fieldLocations.Length - 1 ? _fieldLocations[i].Item2 : fields.Length - _fieldLocations[i].Item1);
+
+            return new ContainerInfo
+            {
+                ContainerId = getField(0).Trim(),
+                Image = getField(1).Trim(),
+                Command = getField(2).Trim(),
+                Created = getField(3).Trim(),
+                Status = getField(4).Trim(),
+                Ports = getField(5).Trim(),
+                Names = getField(6).Trim().Split(',').ToList()
+            };
+        }
+    }
+}
--- src/LclDckr/DockerClient.cs
+++ src/LclDckr/DockerClient.cs
@@ -1,5 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
+using LclDckr.Commands.Ps;
+using LclDckr.Commands.Ps.Filters;
 
 namespace LclDckr
 {
@@ -21,6 +25,14 @@ namespace LclDckr
                 .Last();
         }
 
+        /// <summary>
+        /// Runs the specified image in a new container
+        /// </summary>
+        /// <param name="imageName"></param>
+        /// <param name="name"></param>
+        /// <param name="hostName"></param>
+        /// <param name="interactive"></param>
+        /// <returns>The long uuid of the created container</returns>
         public string RunImage(string imageName, string name, string hostName = null, bool interactive = false)
         {
             var hostArg = hostName != null ? $"--hostname {hostName}" : "";
@@ -36,7 +48,12 @@ namespace LclDckr
             return process.StandardOutput.ReadToEnd();
         }
 
-        public void PullImage(string imageName, string tag)
+        /// <summary>
+        /// Pulls the specified image.
+        /// </summary>
+        /// <param name="imageName"></param>
+        /// <param name="tag"></param>
+        public void PullImage(string imageName, string tag = "latest")
         {
             var args = $"pull {imageName}:{tag}";
 
@@ -46,6 +63,11 @@ namespace LclDckr
             process.ThrowForError();
         }
 
+        /// <summary>
+        /// starts an existing container by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>the name of the started container</returns>
         public string StartContainer(string name)
         {
             var args = $"start {name}";
@@ -58,6 +80,11 @@ namespace LclDckr
             return process.StandardOutput.ReadToEnd();
         }
 
+        /// <summary>
+        /// stops an existing container by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>the name of the stopped container</returns>
         public string StopContainer(string name)
         {
             var args = $"stop {name}";
@@ -69,6 +96,11 @@ namespace LclDckr
             return process.StandardOutput.ReadToEnd();
         }
 
+        /// <summary>
+        /// removes an existing container by name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public string RemoveContainer(string name)
         {
             var args = $"rm {name}";
@@ -78,6 +110,51 @@ namespace LclDckr
             process.ThrowForError();
 
             return process.StandardOutput.ReadToEnd();
+        }
+
+        /// <summary>
+        /// Returns info on this systems containers
+        /// </summary>
+        /// <param name="all">true for all containers, false for running containers only</param>
+        /// <param name="filters"></param>
+        /// <returns></returns>
+        public ICollection<ContainerInfo> Ps(bool all = false, IEnumerable<IFilter> filters = null)
+        {
+            var args = new StringBuilder("ps");
+
+            if (all)
+            {
+                args.Append(" -a");
+            }
+
+            if (filters != null)
+            {
+                foreach (var filter in filters)
+                {
+                    args.Append($" --filter \"{filter.Value}\"");
+                }
+            }
+
+            var process = GetDockerProcess(args.ToString());
+
+            process.Start();
+            process.WaitForExit();
+            process.ThrowForError();
+
+            var headers = process.StandardOutput.ReadLine();
+
+            var parser = new ContainerInfoParser(headers);
+
+            var containers = new List<ContainerInfo>();
+
+            while (!process.StandardOutput.EndOfStream)
+            {
+                var fields = process.StandardOutput.ReadLine();
+                var container = parser.Parse(fields);
+                containers.Add(container);
+            }
+
+            return containers;
         }
 
         private Process GetDockerProcess(string arguments)
