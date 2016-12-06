356e7dbc19f54d589e0ae99b939892d7e74ddd91
derrickcrowne <erickeithbrown@gmail.com>
2016-12-06 14:33:27 -0800

    Added support for environment args and volumes in run command. (#5)

---
+++ src/LclDckr/Commands/Run/RunArguments.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LclDckr.Commands.Run
+{
+    public class RunArguments
+    {
+        public bool Interactive { get; set; }
+
+        public string Name { get; set; }
+
+        public string HostName { get; set; }
+
+        public IDictionary<string, string> EnvironmentArgs = new Dictionary<string, string>();
+
+        public IList<string> Volumes = new List<string>();
+
+        public string ToArgString()
+        {
+            var args = new StringBuilder("-d");
+
+            if (Interactive)
+            {
+                args.Append("i");
+            }
+
+            if (Name != null)
+            {
+                args.Append($" --name {Name}");
+            }
+
+            if (HostName != null)
+            {
+                args.Append($" --hostname {HostName}");
+            }
+
+            foreach (var environmentArg in EnvironmentArgs)
+            {
+                args.Append($" -e {environmentArg.Key}={environmentArg.Value}");
+            }
+
+            foreach (var volume in Volumes)
+            {
+                args.Append($" -v {volume}");
+            }
+
+            return args.ToString();
+        }
+    }
+}
